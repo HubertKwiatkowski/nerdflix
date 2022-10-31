a3cb2ff72153bca44bf54e92371b608fffc2e8a8
warlock <h.kwiatkowski@hotmail.com>
2022-10-31 16:07:39 +0100

    generating all movie position and styling

--- src/main.js
+++ src/main.js
@@ -1,22 +1,26 @@
 async function getMovieInfo() {
   let obj;
   const movie = document.getElementById('movies-display')
-
   const res = await fetch('src/imdb-top-50.json')
   obj = await res.json();
 
-  title = obj.data.movies[0].title
-  year = obj.data.movies[0].year
-  image = obj.data.movies[0].urlPoster
+  for (let i = 0; i < obj.data.movies.length; i++) {
+    title = obj.data.movies[i].title
+    year = obj.data.movies[i].year
+    image = obj.data.movies[i].urlPoster
+    movie.innerHTML += `
+      <div class="single-movie">
+        <div class="movie-image"><img src="${image}" alt="${title} cover">
+        </div>
+        <div class="movie-description">
+          <div class="movie-title">${title}</div>
+          <div class="movie-year">${year}</div>
+        </div>
+      </div>
+    `
+  }
 
-  return (movie.innerHTML = `
-  <div>
-    <div class="movie-image"><img src="${image}" alt="${title} cover">
-    </div>
-    <div class="movie-title">${title}</div>
-    <div class="movie-year">${year}</div>
-  </div>
-  `)
+  return movie.innerHTML
 }
 
-getMovieInfo();
+getMovieInfo()
